8f3eaad26c884fe06edb05788778f0a31fb79f89
Jianglinglong <2899297045@qq.com>
2017-08-29 23:23:15 +0800

    Signed-off-by: Jianglinglong <2899297045@qq.com>

--- JiangUtil/src/com/jll/util/TestFile.java
+++ JiangUtil/src/com/jll/util/TestFile.java
@@ -6,30 +6,34 @@ import java.util.LinkedList;
 public class TestFile {
 	public static void main(String[] args) {
 		LinkedList<String> files = new LinkedList<String>();
-		String srcDir  = "E:\\姜玲珑\\";
-		String dir = "e:\\test";
+		String srcDir = "D:\\【猴年新春特别版】问道怀旧单机";
+		String dir = "d:\\问道怀旧单机";
 		File srcFile = new File(srcDir);
 		File destFile = new File(dir);
 		String srcFilePath = srcFile.getAbsolutePath();
 		String destFilePath = destFile.getAbsolutePath();
 		if (srcFile.exists()) {
-			FileControls.getAllFiles(srcFilePath, files);
-			int total = files.size();
-			int copy = 0;
-			for (String file : files) {
-				copy++;
-				System.out.println("移动:" + ((double) copy / total)*100 + "%");
-				int index = file.indexOf(srcFilePath) + srcFilePath.length();
-				String str = file.substring(index);
-				String dest = destFilePath + str;
-				if(!dest.equals(destFilePath)) {
-					FileControls.moveFile(file, dest);
-				}else {
-					FileControls.deletFile(file);
+			if (destFile.exists() || destFile.mkdirs()) {
+				FileControls.getAllFiles(srcFilePath, files);
+				int total = files.size();
+				int copy = 0;
+				for (String file : files) {
+					copy++;
+					System.out.println("移动:" +(int) (((double) copy / total) * 100) + "%");
+					int index = file.indexOf(srcFilePath) + srcFilePath.length();
+					String str = file.substring(index);
+					String dest = destFilePath + str;
+					if (!dest.equals(destFilePath)) {
+						FileControls.moveFile(file, dest);
+					} else {
+						FileControls.deletFile(file);
+					}
 				}
+			}else {
+				System.out.println("目标文件\""+destFile.getAbsolutePath()+"\"异常");
 			}
-		}else {
-			System.out.println(srcDir+"不存在");
+		} else {
+			System.out.println("源文件\"" + srcFile.getAbsolutePath() + "\"不存在");
 		}
 	}
 }
