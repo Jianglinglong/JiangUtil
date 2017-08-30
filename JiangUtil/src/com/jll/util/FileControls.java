029f1d03552ca66bd594ff381695a861647c9089
姜玲珑 <2899297045@Direct-PC>
2017-08-30 17:39:45 +0800

    Signed-off-by: 姜玲珑 <2899297045@Direct-PC>

--- JiangUtil/src/com/jll/util/FileControls.java
+++ JiangUtil/src/com/jll/util/FileControls.java
@@ -7,8 +7,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -34,7 +35,6 @@ public class FileControls {
 			this.fileSize = this.filePath.length();
 		}
 	}
-
 	public static boolean deletFile(String filePath) {
 		return deletFile(new File(filePath));
 	}
@@ -218,7 +218,6 @@ public class FileControls {
 			files.add(file.getAbsolutePath());
 		}
 	}
-
 	public static void getAllFiles(File srcPath, LinkedList<File> files) {
 		if (srcPath.exists()) {
 			File[] fileList = srcPath.listFiles();
@@ -281,7 +280,100 @@ public class FileControls {
 				fileSize.setFilePath(file.getAbsolutePath());
 				files.add(fileSize);
 			}
-			// files.add(file.getAbsolutePath());
 		}
+	}
+	@SuppressWarnings("unchecked")
+	public static boolean upPackge(String packageFilePath) {
+		File packageFile = new File(packageFilePath);
+		FileInputStream fin = null;
+		ObjectInputStream oin = null;
+		if (packageFile.exists()) {
+			try {
+				fin = new FileInputStream(packageFile);
+				oin = new ObjectInputStream(fin);
+				ArrayList<FileControls.Filesize> list = new ArrayList<FileControls.Filesize>();
+				list = (ArrayList<FileControls.Filesize>) oin.readObject();
+				if (list.size() != 0) {
+					for (int i = 0; i < list.size(); i++) {
+						File file = list.get(i).getFilePath();
+						if (!file.isDirectory()) {
+							String dir = packageFile.getParent()+"\\" + file.getName();
+							File destfile = new File(dir);
+							destfile.getParentFile().mkdirs();
+							FileOutputStream fout = new FileOutputStream(destfile);
+							long filesize = list.get(i).getFileSize();
+							byte[] buffer = new byte[4096];
+							while (true && filesize > 0) {
+								if (filesize > 4096) {
+									fin.read(buffer);
+									fout.write(buffer);
+									filesize -= 4096;
+								} else {
+									fin.read(buffer, 0, (int) filesize);
+									fout.write(buffer, 0, (int) filesize);
+									break;
+								}
+							}
+							fout.flush();
+							fout.close();
+						}else {
+							file.mkdirs();
+						}
+					}
+					oin.close();
+					return true;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return false;
+	}
+
+	public static String PACKAGE_TYPE=".pakge";
+	
+	public static boolean packageFile(String srcDir, String dir) {
+		File srcFile = new File(srcDir);
+		File destFile = new File(dir + PACKAGE_TYPE);
+		String srcFilePath = srcFile.getAbsolutePath();
+		if (srcFile.exists()) {
+				ArrayList<FileControls.Filesize> fileList = new ArrayList<FileControls.Filesize>();
+				FileControls.getAllFiles(srcFilePath, fileList);
+				try {
+					ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(destFile));
+					os.writeObject(fileList);
+					os.flush();
+					os.close();
+					FileOutputStream fs = new FileOutputStream(destFile,true);
+					for (FileControls.Filesize f : fileList) {
+						if (!f.getFilePath().isDirectory()) {
+							FileInputStream fi = new FileInputStream(f.getFilePath());
+							byte[] buffer = new byte[4096];
+							long size = f.getFileSize();
+							while (true) {
+								if (size > 4096) {
+									fi.read(buffer);
+									fs.write(buffer);
+									size -= 4096;
+								} else {
+									fi.read(buffer, 0, (int) size);
+									fs.write(buffer, 0, (int) size);
+									fi.close();
+									break;
+								}
+							}
+						}
+					}
+					fs.flush();
+					fs.close();
+					return true;
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		} else {
+			System.out.println("源文件\"" + srcFile.getAbsolutePath() + "\"不存在");
+		}
+		return false;
 	}
 }
--- JiangUtil/src/com/jll/util/TestFile.java
+++ JiangUtil/src/com/jll/util/TestFile.java
@@ -2,24 +2,21 @@ package com.jll.util;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-import java.util.LinkedList;
-
-import com.jll.util.FileControls.Filesize;
 
 public class TestFile {
 	public static void main(String[] args) {
-		String srcDir = "E:\\test";
-		String dir = "E:\\新建文件夹\\t";
+		String srcDir = "E:\\testsrc\\";
+		String dir = "E:\\testdir\\";
 		packageFile(srcDir, dir);
-		upPackge(dir + ".pakge");
+		upPackge(dir + FileControls.PACKAGE_TYPE);
 	}
 
+	@SuppressWarnings("unchecked")
 	public static boolean upPackge(String packageFilePath) {
 		File packageFile = new File(packageFilePath);
 		FileInputStream fin = null;
@@ -27,23 +24,19 @@ public class TestFile {
 		if (packageFile.exists()) {
 			try {
 				fin = new FileInputStream(packageFile);
-				long total = packageFile.length();
-				long read = 0;
 				oin = new ObjectInputStream(fin);
 				ArrayList<FileControls.Filesize> list = new ArrayList<FileControls.Filesize>();
 				list = (ArrayList<FileControls.Filesize>) oin.readObject();
 				if (list.size() != 0) {
-					for (int i = list.size(); i > 0; i--) {
-						File file = list.get(i - 1).getFilePath();
+					for (int i = 0; i < list.size(); i++) {
+						File file = list.get(i).getFilePath();
 						if (!file.isDirectory()) {
 							String dir = packageFile.getParent()+"\\" + file.getName();
 							File destfile = new File(dir);
 							destfile.getParentFile().mkdirs();
 							FileOutputStream fout = new FileOutputStream(destfile);
-							long filesize = list.get(i - 1).getFileSize();
+							long filesize = list.get(i).getFileSize();
 							byte[] buffer = new byte[4096];
-							read += filesize;
-							fin.skip(total - read);
 							while (true && filesize > 0) {
 								if (filesize > 4096) {
 									fin.read(buffer);
@@ -61,9 +54,10 @@ public class TestFile {
 							file.mkdirs();
 						}
 					}
+					oin.close();
+					return true;
 				}
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -74,9 +68,7 @@ public class TestFile {
 		File srcFile = new File(srcDir);
 		File destFile = new File(dir + ".pakge");
 		String srcFilePath = srcFile.getAbsolutePath();
-		// String destFilePath = destFile.getAbsolutePath();
 		if (srcFile.exists()) {
-			if (true/* destFile.exists() || destFile.getParentFile().mkdirs() */) {
 				ArrayList<FileControls.Filesize> fileList = new ArrayList<FileControls.Filesize>();
 				FileControls.getAllFiles(srcFilePath, fileList);
 				try {
@@ -84,17 +76,17 @@ public class TestFile {
 					os.writeObject(fileList);
 					os.flush();
 					os.close();
-					FileOutputStream fs = new FileOutputStream(destFile, true);
+					FileOutputStream fs = new FileOutputStream(destFile,true);
 					for (FileControls.Filesize f : fileList) {
 						if (!f.getFilePath().isDirectory()) {
 							FileInputStream fi = new FileInputStream(f.getFilePath());
-							byte[] buffer = new byte[1024];
+							byte[] buffer = new byte[4096];
 							long size = f.getFileSize();
 							while (true) {
-								if (size > 1024) {
+								if (size > 4096) {
 									fi.read(buffer);
 									fs.write(buffer);
-									size -= 1024;
+									size -= 4096;
 								} else {
 									fi.read(buffer, 0, (int) size);
 									fs.write(buffer, 0, (int) size);
@@ -110,9 +102,6 @@ public class TestFile {
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-			} else {
-				System.out.println("目标文件\"" + destFile.getAbsolutePath() + "\"异常");
-			}
 		} else {
 			System.out.println("源文件\"" + srcFile.getAbsolutePath() + "\"不存在");
 		}
